c0503fc8e5b0c4e7e9867c7146225e7f9fadd367
Max <maksimtetuskin1267@mail.ru>
2023-12-22 03:39:44 +0700

    qtree was created

--- qtree.py
+++ qtree.py
@@ -1,14 +1,21 @@
 import threading
 from PIL import Image, ImageDraw
 
-PADDING = 0
-OUTPUT_SCALE = 1
 ERROR_THRESHOLD = 7
 
 
-class Quadrant:
+class QuadNode:
 
     def __init__(self, image: Image.Image, box, depth):
+
+        """
+        Создание узла квадродерева на изображении
+
+        :param image: изображение
+        :param box:
+        :param depth: глубина узла
+        """
+
         self.box = box
         self.depth = depth
         self.children = None
@@ -17,10 +24,16 @@ class Quadrant:
         image = image.crop(box)
         self.width, self.height = image.size
         hist = image.histogram()
-        self.color, self.error = Quadrant.color_from_histogram(hist)
+        self.color, self.error = QuadNode.get_colors(hist)
 
     @staticmethod
-    def weighted_average(hist):
+    def color_average(hist):
+        """
+        Вычисление среднего значение для каждого цвета и его ошибки
+
+        :param hist: Часть гистограммы где находятся данные про rgb
+        :return:
+        """
         total = sum(hist)
         value, error = 0, 0
         if total > 0:
@@ -30,24 +43,63 @@ class Quadrant:
         return value, error
 
     @staticmethod
-    def color_from_histogram(hist):
-        r, re = Quadrant.weighted_average(hist[:256])
-        g, ge = Quadrant.weighted_average(hist[256:512])
-        b, be = Quadrant.weighted_average(hist[512:768])
+    def get_colors(hist):
+
+        """
+        Получение среднего цвета в квадранте и ошибки
+
+        :param hist:
+        :return:
+        """
+
+        r, re = QuadNode.color_average(hist[:256])
+        g, ge = QuadNode.color_average(hist[256:512])
+        b, be = QuadNode.color_average(hist[512:768])
         e = re * 0.2989 + ge * 0.5870 + be * 0.1140
         return (int(r), int(g), int(b)), e
 
+    @property
     def is_leaf(self):
+
+        """
+        Указание на то, что узел лист, т.е является листом с максимальной глубиной
+        в квадродереве, либо ошибка цвета переходит за порог ошибки
+
+        :return:
+        """
+
         return self.leaf
 
+    @is_leaf.setter
+    def is_leaf(self, leaf: bool):
+
+        """
+        Сеттер для is_leaf
+
+        :param leaf: Новое значение для self.leaf
+        :return:
+        """
+
+        self.leaf = leaf
+
     def split(self, image):
+
+        """
+        Разделение родительского квадранта узла на 4 квадранта
+
+        :param image: Часть изображения, где происходит разбитие
+        :return:
+        """
+
         left, top, right, bottom = self.box
         left_right = left + (right - left) // 2
         top_bottom = top + (bottom - top) // 2
-        north_west = Quadrant(image, (left, top, left_right, top_bottom), self.depth + 1)
-        nort_east = Quadrant(image, (left_right, top, right, top_bottom), self.depth + 1)
-        south_west = Quadrant(image, (left, top_bottom, left_right, bottom), self.depth + 1)
-        south_east = Quadrant(image, (left_right, top_bottom, right, bottom), self.depth + 1)
+
+        north_west = QuadNode(image, (left, top, left_right, top_bottom), self.depth + 1)
+        nort_east = QuadNode(image, (left_right, top, right, top_bottom), self.depth + 1)
+        south_west = QuadNode(image, (left, top_bottom, left_right, bottom), self.depth + 1)
+        south_east = QuadNode(image, (left_right, top_bottom, right, bottom), self.depth + 1)
+
         self.children = [
             north_west,
             nort_east,
@@ -57,83 +109,125 @@ class Quadrant:
 
 
 class QTree:
-    def __init__(self, image: Image.Image, max_depth=1024):
-        self.root = Quadrant(image, image.getbbox(), 0)
+    def __init__(self, image: Image.Image, max_depth=None):
+
+        """
+        Создание квадродерева, способного сживать изображение
+
+        :param image: Изображение для сжатия
+        :param max_depth: Глубина, до которого хотим сжать изображение
+        """
+
+        self.root = QuadNode(image, image.getbbox(), 0)
         self.width, self.height = image.size
+        if max_depth is None:
+            self.max_depth = 1024
+        self.build(image, self.root, max_depth)
 
-        self.max_depth = 0
-        self.build_tree(image, self.root, max_depth)
+    def build(self, image, node, max_depth):
 
-    def build_tree(self, image, node, max_depth):
-        """Recursively adds nodes untill max_depth is reached or error is less than 5"""
+        """
+        Рекурсивное построение квадродерева
+
+        :param image: путь к изображению
+        :param node: корневой узел в квадранте изображения
+        :param max_depth: Максимальная глубина построения дерева
+        :return:
+        """
+
         if (node.depth >= max_depth) or (node.error <= ERROR_THRESHOLD):
             if node.depth > self.max_depth:
                 self.max_depth = node.depth
-            node.leaf = True
+            node.is_leaf = True
             return
 
         node.split(image)
-        if node == 0:
+
+        if node.depth == 0:
             threaders = []
+
             for child in node.children:
-                thread = threading.Thread(target=self.build_tree, args=(image, child, max_depth))
+                thread = threading.Thread(target=self.build, args=(image, child, max_depth))
                 threaders.append(thread)
                 thread.start()
+
             for thread in threaders:
                 thread.join()
+
         else:
             for child in node.children:
-                self.build_tree(image, child, max_depth)
+                self.build(image, child, max_depth)
 
-    def get_leaf_nodes(self, depth):
-        """Gets all the nodes on a given depth/level"""
+    def get_nodes(self, depth):
 
-        def get_leaf_nodes_recusion(tree, node, depth, func):
-            """Recusivley gets leaf nodes based on whether a node is a leaf or the given depth is reached"""
-            if node.leaf is True or node.depth == depth:
-                func(node)
-            elif node.children is not None:
-                for child in node.children:
-                    get_leaf_nodes_recusion(tree, child, depth, func)
+        """
+        Полечение списка узлов, глубиной меньше depth
+
+        :param depth: глубина, до которой нам нужны узла дерева
+        :return: список узлов квадродерево
+        """
 
         if depth > self.max_depth:
             raise ValueError('A depth larger than the trees depth was given')
 
-        leaf_nodes = []
-        get_leaf_nodes_recusion(self, self.root, depth, leaf_nodes.append)
-        return leaf_nodes
+        nodes = []
+        QTree.search_nodes(self, self.root, depth, nodes.append)
+        return nodes
 
-    def create_image_from_depth(self, depth, show_tree = False):
-        """Creates a Pillow image object from a given level/depth of the tree"""
-        m = OUTPUT_SCALE
-        dx, dy = (PADDING, PADDING)  # padding for each image section
-        image = Image.new('RGB', (int(self.width * m + dx),
-                                  int(self.height * m + dy)))
+    @staticmethod
+    def search_nodes(tree, node, depth, func):
+
+        """
+        Рекурсивное получение узлов квадродерева
+
+        :param tree: квадродерево
+        :param node: текущий узел
+        :param depth: глубина узла
+        :param func: функция, добавляющая узел в список узлов
+        :return:
+        """
+
+        if node.is_leaf is True or node.depth == depth:
+            func(node)
+        elif node.children is not None:
+            for child in node.children:
+                QTree.search_nodes(tree, child, depth, func)
+
+    def create_image(self, depth):
+
+        """
+        Создание сжатого изображения, учитывая глубину сжатия
+        :param depth: глубина сжатия
+        :return: Сжатое изображение
+        """
+
+        image = Image.new('RGB', (int(self.width), int(self.height)))
         draw = ImageDraw.Draw(image)
-        draw.rectangle((0, 0, self.width * m + dx,
-                        self.height * m + dy), (0, 0, 0))
+        draw.rectangle((0, 0, self.width, self.height), (0, 0, 0))
 
-        leaf_nodes = self.get_leaf_nodes(depth)
-        for node in leaf_nodes:
-            l, t, r, b = node.box
-            box = (l * m + dx, t * m + dy, r * m - 1, b * m - 1)
+        nodes = self.get_nodes(depth)
+
+        for node in nodes:
+            left, top, right, bottom = node.box
+            box = (left, top, right, bottom)
             draw.rectangle(box, node.color)
         return image
 
-    def render_at_depth(self, depth=0):
-        """Renders the image of a given depth/level"""
-        if depth > self.max_depth:
-            raise ValueError('A depth larger than the trees depth was given')
-
-        image = self.create_image_from_depth(depth)
-        image.show()
-
     def create_gif(self, file_name, duration=1000, loop=0):
-        """Creates a gif at the given filename from each level of the tree"""
+
+        """
+        Создание gif-файла
+
+        :param file_name: путь к изображение
+        :param duration: Число циклв в gif-файле
+        :param loop:
+        :return:
+        """
+
         images = []
-        end_product_image = self.create_image_from_depth(self.max_depth)
+        end_product_image = self.create_image(self.max_depth)
         for i in range(self.max_depth):
-            image = self.create_image_from_depth(i)
+            image = self.create_image(i)
             images.append(image)
         for _ in range(4):
             images.append(end_product_image)
@@ -142,4 +236,3 @@ class QTree:
             save_all=True,
             append_images=images[1:],
             duration=duration, loop=loop)
-
